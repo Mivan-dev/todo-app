2050916b62d543510e3f7a8071bad109f56f8a84
Ivan-M <sirius.ivo@gmail.com>
2025-09-17 22:38:21 -0300

    feature: update content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toolbar } from './layout/toolbar/toolbar';
+import { Footer } from "./layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
