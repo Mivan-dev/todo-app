df2207613e658f637d3b1d8d5814145a98d2f6ff
Ivan-M <sirius.ivo@gmail.com>
2025-09-19 22:48:59 -0300

    feature: add view task component

---
+++ src/app/features/agregar-tarea/agregar-tarea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-tarea',
+  imports: [],
+  templateUrl: './agregar-tarea.html',
+  styleUrl: './agregar-tarea.scss'
+})
+export class AgregarTarea {
+
+}
