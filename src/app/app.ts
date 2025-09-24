e01a844e7d23490969baaa1536031157d79f2d35
Ivan-M <sirius.ivo@gmail.com>
2025-09-24 16:47:27 -0300

    feature: add tareas component update

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toolbar } from './layout/toolbar/toolbar';
 import { Footer } from "./layout/footer/footer";
+import { Tareas } from "./features/tareas/tareas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toolbar, Footer],
+  imports: [RouterOutlet, Toolbar, Footer, Tareas],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
