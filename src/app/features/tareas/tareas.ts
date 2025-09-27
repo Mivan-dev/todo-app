bf26f57f7240f1f68ac30f1c9098aab310463561
Ivan-M <sirius.ivo@gmail.com>
2025-09-27 17:51:07 -0300

    feature: enhance tareas component with dynamic task rendering and styling

--- src/app/features/tareas/tareas.ts
+++ src/app/features/tareas/tareas.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from '../../shared/entities';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TareasBd } from '../../shared/tareas-bd/tareas-bd';
 
 @Component({
   selector: 'app-tareas',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './tareas.html',
   styleUrl: './tareas.scss'
 })
-export class Tareas {
+export class Tareas implements OnInit {
+  tareas: Tarea[] = [];
+
+  constructor(private tareasBd: TareasBd) { }
+
+  ngOnInit(): void {
+    //Cargar tareas al iniciar el componente
+    this.tareas = this.tareasBd.getTareas();
+    //Para limpiar las tareas del localStorage
+    //localStorage.clear();
+  }
 
 }
