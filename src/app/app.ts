bdf41492762d1cd6328546766434cd683dda8c64
Ivan-M <sirius.ivo@gmail.com>
2025-09-27 21:52:34 -0300

    feature: integrate AgregarTarea component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Toolbar } from './layout/toolbar/toolbar';
 import { Footer } from "./layout/footer/footer";
 import { Tareas } from "./features/tareas/tareas";
+import { AgregarTarea } from "./features/agregar-tarea/agregar-tarea";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toolbar, Footer, Tareas],
+  imports: [RouterOutlet, Toolbar, Footer, Tareas, AgregarTarea],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
