684cd99937f03ba6d3c0c872969f0eba42c02bb2
Ivan-M <sirius.ivo@gmail.com>
2025-10-01 01:36:35 -0300

    feature: refactor toolbar layout and integrate AgregarTarea component

--- src/app/layout/toolbar/toolbar.ts
+++ src/app/layout/toolbar/toolbar.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AgregarTarea } from '../../features/agregar-tarea/agregar-tarea';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [CommonModule, AgregarTarea],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss'
 })
 export class Toolbar {
+  constructor() { }
 
 }
